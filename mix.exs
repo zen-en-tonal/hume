5e9614ea88b1d9cf2038804eb7245a0a13592d95
Takeru KODAMA <zen.en.tonal@gmail.com>
2025-10-03 12:12:54 +0000

    Add LICENSE file and update mix.exs with project metadata

--- mix.exs
+++ mix.exs
@@ -1,13 +1,23 @@
 defmodule Hume.MixProject do
   use Mix.Project
 
+  @version "0.0.1"
+
   def project do
     [
       app: :hume,
-      version: "0.0.0",
+      version: @version,
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "Event-sourced state machines for Elixir",
+      package: package(),
+      source_url: "https://github.com/zen-en-tonal/hume",
+      homepage_url: "https://github.com/zen-en-tonal/hume",
+      docs: [
+        main: "Hume",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -27,6 +37,18 @@ defmodule Hume.MixProject do
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:stream_data, "~> 0.6", only: :test}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Takeru KODAMA"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/zen-en-tonal/hume",
+        "Docs" => "https://hexdocs.pm/hume"
+      },
+      files: ~w(lib mix.exs README.md LICENSE)
     ]
   end
 end
